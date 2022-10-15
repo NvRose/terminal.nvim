672c08e2338e50ea64cd29877b30c526d61c2bfc
fenze <contact@fenze.dev>
2022-10-16 00:07:49 +0200

    initial commit

---
+++ .github/readme.md
@@ -0,0 +1,44 @@
+<h3 align=center> NvRose - terminal </h3>
+
+### Installation
+
+```lua
+use {
+	"NvRose/terminal",
+ 	config = function()
+		require("terminal").setup()
+	end
+}
+```
+
+### Configuration
+
+```lua
+require('terminal').setup {
+	shell = vim.o.shell,
+	resize = 15,
+   	orientation = "horizontal",
+    	keybinds = {
+		-- Inside terminal
+		inside = {
+			["<c-\\>"] = term.close,
+		},
+		-- In normal mode
+		outside = {
+			["<c-\\>"] = term.new
+		}
+    	},
+    	behavior = {
+		close_on_exit = true,
+	   	auto_insert = true
+    	}
+}
+```
+
+### 📜 License
+NvRose is released under own license, which grants the following permissions:
+- Commercial use
+- Distribution
+- Modification
+- Private use
+For more details see [license](https://github.com/NvRose/terminal/license).
---
+++ lua/terminal/init.lua
@@ -0,0 +1,92 @@
+local M = {}
+local opts = {}
+
+local set_behavior = function(behavior)
+	if behavior.close_on_exit then
+		vim.api.nvim_create_autocmd("TermClose", {
+			callback = function()
+				vim.schedule_wrap(vim.api.nvim_input "<cr>")
+			end
+		})
+	end
+
+	if behavior.auto_insert then
+		vim.api.nvim_create_autocmd("BufEnter", {
+			callback = function()
+				vim.cmd 'start'
+			end,
+			pattern = "term://*"
+		})
+
+		vim.api.nvim_create_autocmd("BufLeave", {
+			callback = function()
+				vim.cmd 'stopi'
+			end,
+			pattern = "term://*"
+		})
+	end
+end
+
+local set_keybinds = function(keybinds)
+	for k, v in pairs(keybinds.inside) do
+		vim.keymap.set('t', k, function()
+			return v()
+		end)
+	end
+
+	for k, v in pairs(keybinds.outside) do
+		vim.keymap.set('n', k, function()
+			return v()
+		end)
+	end
+end
+
+M.new = function()
+	if M.is_open then
+		return M.close()
+	end
+
+	if opts.orientation == "horizontal" then
+		vim.cmd('sp term://' .. opts.shell .. ' | res ' .. opts.resize)
+	elseif opts.orientation == "vertical" then
+		vim.cmd('vs term://' .. opts.shell .. ' | vert res ' .. opts.resize)
+	end
+
+	M.is_open   = true
+	vim.wo.rnu  = false
+	vim.wo.nu   = false
+	vim.wo.cul  = false
+	vim.wo.list = false
+end
+
+M.close = function()
+	vim.cmd('bd! term://*')
+	M.is_open = false
+end
+
+local defaults = {
+    shell = vim.o.shell,
+    resize = 15,
+    orientation = "horizontal",
+    keybinds = {
+	inside = {
+		["<c-\\>"] = M.close,
+	},
+	outside = {
+		["<c-\\>"] = M.new
+	}
+    },
+    behavior = {
+	    close_on_exit = true,
+	    auto_insert = true
+    }
+}
+
+M.setup = function(config)
+	config = config and vim.tbl_deep_extend("force", defaults, config) or defaults
+	set_behavior(config.behavior)
+	set_keybinds(config.keybinds)
+	opts = config
+end
+
+return M
